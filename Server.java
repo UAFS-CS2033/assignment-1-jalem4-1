c3c9880225bd030b8d969bac68fbded78a65f07e
jalem4 <144487241+jalem4@users.noreply.github.com>
2024-09-17 22:12:00 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,19 +16,49 @@ public class Server{
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
 
         //*** Application Protocol *****
-        String buffer = in.readLine();
-        while(!(buffer.equals("quit"))){
-            out.println("From Server: " + buffer);
-            buffer = in.readLine();
+        String request = in.readLine();
+
+        String[] requestParts = request.split(" ");
+        String path = requestParts[1];
+        path = "docroot" + path;
+        File file = new File(path);
+        if (file.exists()){
+
+            out.println("HTTP/1.1 " + 200 + " " + "OK");
+            out.println("Content-Type: " + getContentType(path));
+            out.println("Content-Length: " + file.length());
+            out.println("Connection: close");
+            out.println(); // Blank line to separate headers from body
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line = br.readLine()) != null){
+                out.println(line);
+            }
         }
-       
+        else{
+            out.println("Not a file");
+            return;
+        }
+
         in.close();
         out.close();
     }
 
+    public static String getContentType(String filePath) {
+        if (filePath.endsWith(".html")) return "text/html";
+        if (filePath.endsWith(".css")) return "text/css";
+        if (filePath.endsWith(".js")) return "application/javascript";
+        if (filePath.endsWith(".png")) return "image/png";
+        if (filePath.endsWith(".jpg") || filePath.endsWith(".jpeg")) return "image/jpeg";
+        if (filePath.endsWith(".gif")) return "image/gif";
+        return "text/plain"; // Default type
+    }
+    
+
+
     public void run() throws IOException{
         boolean running = true;
-       
+
         serverSocket = new ServerSocket(portNo);
         System.out.printf("Listen on Port: %d\n",portNo);
         while(running){
